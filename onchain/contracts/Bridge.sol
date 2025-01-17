200bdc02b215d67f090f59cd6c803f9bdfa2bea5
samokander <andrei.samokish111@gmail.com>
2025-01-17 18:40:00 +0700

    feat: bridge deposit functionality

--- onchain/contracts/Bridge.sol
+++ onchain/contracts/Bridge.sol
@@ -2,20 +2,41 @@
 pragma solidity 0.8.28;
 
 import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/structs/MerkleTree.sol";
+import "./Poseidon.sol";
 
 contract Bridge {
     using SafeERC20 for IERC20;
+    using MerkleTree for MerkleTree.Bytes32PushTree;
 
-    IERC20 immutable token;
-    uint immutable portion;
+    error Bridge__ExistentCommitment(bytes32 commitment);
+
+    bytes32 constant ZERO_VALUE = keccak256("bridge");
+    uint8 constant TREE_DEPTH = 20;
+
+    IERC20 public immutable token;
+    uint public immutable portion;
+
+    MerkleTree.Bytes32PushTree private tree;
+
+    mapping(bytes32 => bool) public commitments;
 
     constructor(address _token, uint _portion) {
         token = IERC20(_token);
         portion = _portion;
+
+        tree.setup(TREE_DEPTH, ZERO_VALUE, PoseidonT3.poseidon);
     }
 
-    function deposit() public {
+    function deposit(bytes32 _commitment) public {
+        require(
+            !commitments[_commitment],
+            Bridge__ExistentCommitment(_commitment)
+        );
+
         token.safeTransferFrom(msg.sender, address(this), portion);
+
+        tree.push(_commitment, PoseidonT3.poseidon);
 
         // todo: add to merkle tree
     }
