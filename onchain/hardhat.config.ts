0fb3b1eb812e45bf83230464e98b446e0059d5b8
samokander <andrei.samokish111@gmail.com>
2025-01-20 15:20:45 +0700

    update: solidity optimizer

--- onchain/hardhat.config.ts
+++ onchain/hardhat.config.ts
@@ -1,8 +1,16 @@
-import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config";
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.28",
+	solidity: {
+		version: "0.8.28",
+		settings: {
+			optimizer: {
+				enabled: true,
+				runs: 999999,
+			},
+		},
+	},
 };
 
 export default config;
